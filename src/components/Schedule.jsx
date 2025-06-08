1d4042a6a3e1bb642f6743b5945e80facee01b36
HwangSangIm <apples1243@dongyang.ac.kr>
2025-06-08 12:02:32 +0900

    Update Schedule.jsx

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -5,9 +5,9 @@ function Schedule({date,title,desc,time,onchange}) {
         <div>선택한 날짜</div>
         <div>{date}</div>
         <div>일정을 입력하세요.</div>
-        <input type="text" name="title" value={title} onChange={onchange} placeholder={title ? title : "일정 입력"}/>
+        <input type="text" name="title" value={title} onChange={onchange} placeholder={"일정 입력"}/>
         <div>내용을 입력하세요.</div>
-        <input type="text" name="desc" value={desc} onChange={onchange} placeholder={desc ? desc : "내용 입력"}/>
+        <input type="text" name="desc" value={desc} onChange={onchange} placeholder={"내용 입력"}/>
         <div>시간을 정하세요.</div>
         <input type="time" name="time" value={time} onChange={onchange}/>
       </form>
